f596fc75cefba1856b1ba71ed29c7ebaa6e8e683
Victor <victor-likes-coding@tempestas.io>
2023-04-05 22:08:04 -0400

    move server test code into it's own file

--- server/users/router.spec.js
+++ server/users/router.spec.js
@@ -2,29 +2,9 @@ const request = require('supertest');
 const server = require('../../server');
 
 describe('server', () => {
-  describe('[GET] /', () => {
-    it('should return 200 OK', () => {
-      return request(server)
-        .get('/')
-        .then((res) => {
-          expect(res.status).toBe(200);
-        });
-    });
-
-    it('should return JSON', () => {
-      return request(server)
-        .get('/')
-        .then((res) => {
-          expect(res.type).toMatch(/json/i);
-        });
-    });
-
-    it('should return { api: "up" }', () => {
-      return request(server)
-        .get('/')
-        .then((res) => {
-          expect(res.body).toEqual({ api: 'up' });
-        });
+  describe('sanity test', () => {
+    it('should pass', () => {
+      expect(true).toBe(true);
     });
   });
 });
